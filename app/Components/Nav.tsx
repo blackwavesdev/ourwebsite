a788337ddf57d84616bdf9ffbf4f20ff23810020
Ibrahem0Salah1 <ibrahemsalah011@gmail.com>
2024-10-26 12:13:34 +0300

    firstC

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+// import React, { useState } from "react";
 const Nav = () => {
-  return <div>Nav</div>;
+  //Navigations
+  const links=[
+        {name:'About', link:'/'},
+        {name:'Services', link:'/'},
+        {name:'Pricing', link:'/'},
+        {name:'Blog', link:'/'}
+  ]
+  // const [Menu, setMenu] = useState(false);
+  return <header className="bg-gray-900 text-gray-50 flex  justify-between p-1 box-border items-center">
+  <div className="logo text-center basis-1/5">
+      {/* <img src="" alt="" /> */}
+      <p>black waves</p>
+  </div>
+  <nav className="basis-2/5">
+      <ul className="flex justify-between bg-transparent p-4">
+          {links.map((link ,index)=>(
+              <li key={index}>
+                  <a href={link.link}>{link.name}</a>
+              </li>
+          ))
+          }
+      </ul>
+  </nav>
+  <div className="contact text-center basis-1/5">
+      <p>Contact</p>
+  </div>
+  <div className="menuBtn">
+
+  </div>
+</header>;
 };
 
 export default Nav;
