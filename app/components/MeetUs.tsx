0cb6f56dc1613683f8972a9183a03a3354e77b72
ghareebOoo <ghareebmohamed406@gmail.com>
2024-11-01 06:08:39 +0200

    fontAdjust

--- app/components/MeetUs.tsx
+++ app/components/MeetUs.tsx
@@ -4,9 +4,9 @@ const MeetUs = () => {
   return (
   <div className='bg-black h-screen'>
       <div className='w-full md:w-[80%] m-auto text-center flex flex-col justify-center h-full'>
-        <span className='text-main text-5xl'>Meet<span className='ml-2 text-white'>Black Waves</span>.</span>
+        <span className='text-main mb-2 text-5xl font-extrabold'>Meet<span className='ml-2 text-white'>Black Waves</span>.</span>
         <h2 className='text-white'>Your Vision Our Code</h2>
-        <div className='mt-10'>
+        <div className='mt-10 px-2'>
             <p className='text-white'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima voluptas, 
                 quas vel perferendis sed id suscipit fugiat distinctio totam corrupti voluptatibus 
