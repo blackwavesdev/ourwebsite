d591458e31ab647c3225cecb24e9d84b0347a214
omarghandour <omarkhaled9913@gmail.com>
2024-11-04 19:12:41 +0200

    adjusting some responsiveness

--- app/components/Welcome/Welcome.tsx
+++ app/components/Welcome/Welcome.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className="bg-black h-screen flex flex-col justify-around text-white">
-      <div className="center flex-col ">
+      <div className="center flex-col">
         <h2 className="font-extrabold">Welcome To</h2>
-        <div className="flex items-end gap-1">
+        <div className="flex items-end">
           <h1 className="text-5xl font-extrabold">Black Waves</h1>{" "}
-          <span className="bg-main rounded-full p-[6px] mb-2"></span>
+          <span className="bg-main p-1 rounded-full mb-2"></span>
         </div>
-        <p className="text-pretty w-[45%] text-center">
+        <p className="text-pretty md:w-[45%] px-2 md:px-0 text-center">
           Where advertising is a Main Course, not a Piece of Cake. We blend
           creativity, strategy, and innovation to serve you delectable results.
         </p>
