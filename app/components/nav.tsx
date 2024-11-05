cdb29dd7b87f143f6323e18ff2929b332a41ea1b
Ibrahem0Salah1 <ibrahemsalah011@gmail.com>
2024-11-05 19:05:18 +0200

    5thCommit

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -102,7 +102,7 @@ const Nav = () => {
             <nav
                 className={`lg:hidden fixed left-0 w-full h-full transition-all  duration-200 ease-in-out bg-black text-center z-50 
                     ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'} 
-                    ${!isHeaderVisible ? 'h-screen top-0' : 'top-20 h-auto'}`}
+                    ${!isHeaderVisible ? 'h-screen top-0' : 'top-24 h-auto'}`}
             >
                 <ul
                     className={`flex flex-col justify-center items-center transition-transform duration-500 ease-in-out 
