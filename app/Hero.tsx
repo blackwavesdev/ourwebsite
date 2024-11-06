9cfd4f46f06d8959044c4c4e32c5ad264cad5f9f
omarghandour <omarkhaled9913@gmail.com>
2024-11-06 18:27:06 +0200

    reviewing code

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import logo from "../Public/logo-bw.png";
 const Hero = () => {
   return (
-    <div className=" relative back bg-cover bg-center bg-fixed h-screen flex items-center justify-center ">
-      <div className="  flex justify-center items-center ">
+    <div className="relative back bg-cover bg-center bg-fixed h-screen flex items-center justify-center ">
+      <div className="flex flex-col md:flex-row justify-center items-center ">
         <Image
           src={logo}
           alt="Hero Image"
@@ -12,7 +12,7 @@ const Hero = () => {
           height={500}
           className="flex justify-center items-center"
         />
-        <p className="text-white text-base md:text-lg lg:text-xl  text-pretty leading-relaxed p-4 md:p-6">
+        <p className="text-white text-base md:text-lg text-center md:text-left lg:text-xl  text-pretty leading-relaxed p-4 md:p-6">
           Digital Marketing Agency, SEO Solutions <br /> Innovative Web
           Devolpment{" "}
         </p>
