3e7bedb84cf534f4853e749875b51e505b55f69d
Mahmoud_Elhosary <140736983+mahmoudelhosarry@users.noreply.github.com>
2024-11-25 21:55:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       </head>
       <body className={` font-Poppins antialiased`}>
         {children}
-        <Fotter />
+        
       </body>
     </html>
   );
