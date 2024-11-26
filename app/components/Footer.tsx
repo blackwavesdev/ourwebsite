c5042fe8c9de2620ebecc0f6e2392122e52ddc5e
Omar <omarkhaled9913@gmail.com>
2024-11-26 08:01:35 +0200

    smooth scrolling from navbar

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -77,10 +77,10 @@ const Footer = forwardRef<HTMLDivElement>((_, ref) => {
             <div className="flex flex-col gap-2">
               <h1 className="text-main text-2xl font-extrabold">Call us now</h1>
               <a
-                href="https://wa.me/213133212321?text=Hello%20there!%20I%20have%20a%20question%20about%20your%20services."
+                href="https://wa.me/01065065760?text=Hello%20there!%20I%20have%20a%20question%20about%20your%20services."
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center justify-center gap-2 bg-transparent text-main px-4 py-2 rounded-md hover:bg-main hover:text-white duration-300"
+                className="flex items-center flex-row justify-center gap-2 bg-transparent text-white px-10 py-2 rounded-md hover:bg-main e duration-300     transition-all ease-out  border-opacity-50  border-2 bg-black outline-1 outline-main border-main font-bold cursor-pointer hover:scale-95"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
