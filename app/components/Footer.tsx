dd87fd05f3340f07ebad4383508ad48ce2c9cc36
Omar <omarkhaled9913@gmail.com>
2024-12-13 17:24:09 +0200

    adjusting the height

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -61,39 +61,38 @@ const Footer = forwardRef<HTMLDivElement>((_, ref) => {
       className="bg-black snap-start text-white h-dvh flex items-end pt-20"
     >
       <div
-        className={`  transition-all duration-700 flex flex-col justify-between h-full ${
+        className={`px-5  transition-all duration-700 flex flex-col justify-between h-full ${
           isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
         }`}
       >
-        <div className="">
-          <div className="">
-            <div className="">
-              <h1 className="">Call us now</h1>
-              <a
-                href="https://wa.me/01065065760?text=Hello%20there!%20I%20have%20a%20question%20about%20your%20services."
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center max-w-sm gap-2 bg-transparent text-white px-10 py-2 rounded-md hover:bg-main hover:text-black transition-all duration-300 border-2 border-main font-bold cursor-pointer hover:scale-95"
+        <div className="flex flex-col gap-3 ">
+          <div className="pl-3 border-l flex flex-col gap-2">
+            {" "}
+            <h1 className="text-[1.5rem] font-bold">Call us now</h1>
+            <a
+              href="https://wa.me/01065065760?text=Hello%20there!%20I%20have%20a%20question%20about%20your%20services."
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center w-1/2 gap-2 bg-transparent text-white px-2 py-2 rounded-md hover:bg-main hover:text-black transition-all duration-300 border-2 border-main font-bold cursor-pointer hover:scale-95"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                className="w-5 h-5"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5"
-                >
-                  <path d="M6.014 8.006c.114-.904 1.289-2.132 2.22-1.996V6.01c.907.172 1.625 1.734 2.03 2.436.286.509.1 1.025-.167 1.243-.361.29-.926.692-.808 1.095C9.5 11.5 12 14 13.23 14.711c.466.269.804-.44 1.092-.804.21-.28.726-.447 1.234-.171.759.442 1.474.956 2.135 1.534.33.276.408.684.179 1.115-.403.76-1.569 1.76-2.415 1.557C13.976 17.587 8 15.27 6.08 8.558c-.108-.318-.08-.438-.066-.552"></path>
-                  <path
-                    fillRule="evenodd"
-                    d="M12 23c-1.224 0-1.9-.131-3-.5l-2.106 1.053A2 2 0 0 1 4 21.763V19.5c-2.153-2.008-3-4.323-3-7.5C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11m-6-4.37-.636-.593C3.691 16.477 3 14.733 3 12a9 9 0 1 1 9 9c-.986 0-1.448-.089-2.364-.396l-.788-.264L6 21.764z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                Chat on WhatsApp
-              </a>
-            </div>
+                <path d="M6.014 8.006c.114-.904 1.289-2.132 2.22-1.996V6.01c.907.172 1.625 1.734 2.03 2.436.286.509.1 1.025-.167 1.243-.361.29-.926.692-.808 1.095C9.5 11.5 12 14 13.23 14.711c.466.269.804-.44 1.092-.804.21-.28.726-.447 1.234-.171.759.442 1.474.956 2.135 1.534.33.276.408.684.179 1.115-.403.76-1.569 1.76-2.415 1.557C13.976 17.587 8 15.27 6.08 8.558c-.108-.318-.08-.438-.066-.552"></path>
+                <path
+                  fillRule="evenodd"
+                  d="M12 23c-1.224 0-1.9-.131-3-.5l-2.106 1.053A2 2 0 0 1 4 21.763V19.5c-2.153-2.008-3-4.323-3-7.5C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11m-6-4.37-.636-.593C3.691 16.477 3 14.733 3 12a9 9 0 1 1 9 9c-.986 0-1.448-.089-2.364-.396l-.788-.264L6 21.764z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              Chat on WhatsApp
+            </a>
           </div>
           <div className="border-l border-main px-3">
-            <h1 className="text-main text-2xl font-extrabold">
+            <h1 className="text-main text-[1.5rem] font-extrabold">
               SEND A MESSAGE
             </h1>
             <p className="text-white font-bold">
@@ -127,7 +126,7 @@ const Footer = forwardRef<HTMLDivElement>((_, ref) => {
           </ul>
         </div>
 
-        <div className="flex flex-col mx-auto space-y-4">
+        <div className="flex flex-col mx-auto">
           <h1 className="text-main font-extrabold text-2xl">NEWSLETTER</h1>
           <form
             onSubmit={handleSubmit}
